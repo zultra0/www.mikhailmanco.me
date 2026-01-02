40b51424d8410f7175884683514154f892347192
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-01 20:44:36 -0500

    Removing unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,3 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Mikhail's website</h1>
-      <h2>Welcome 👋!</h2>
-      <p>Powered by S3, Cloudflare and Github Actions.</p>
-    </>
-  )
+export default function App() {
+  return <h1>Mikhail's website</h1>
 }
-
-export default App

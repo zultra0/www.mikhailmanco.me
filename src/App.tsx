8d5fb121514c7c510fd4fc4506b6a1316f2914ca
Mikhail <mikhail.manco.dev@gmail.com>
2026-01-02 23:53:55 -0500

    Centering content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
-    <>
+    <div className="h-screen w-screen flex justify-center items-center flex-col">
       <h1>Mikhail's website</h1>
-      <h2>Welcome Tailwind!</h2>
-    </>
+      <h1>Welcome Tailwind!</h1>
+    </div>
   )
 }
